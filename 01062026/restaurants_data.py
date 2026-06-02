681b28b11350ba689259e5e4b1aaab4c06b167a1
Александра Рудак <rudak_a@ItTopNSK.local>
2026-06-02 20:02:17 +0700

    разбор дз за 21.05.2026

---
+++ 01062026/restaurants_data.py
@@ -0,0 +1,70 @@
+restaurants_data = [
+    {
+        "name": "Уголек",
+        "cuisine": "русская",
+        "year_opened": 2015,
+        "rating": 4.8,
+        "signature_dishes": [
+            {"name": "борщ", "type": "суп"},
+            {"name": "пельмени", "type": "основное"},
+            {"name": "медовик", "type": "десерт"}
+        ]
+    },
+    {
+        "name": "Сакура",
+        "cuisine": "японская",
+        "year_opened": 2018,
+        "rating": 4.9,
+        "signature_dishes": [
+            {"name": "роллы Филадельфия", "type": "основное"},
+            {"name": "рами", "type": "суп"},
+            {"name": "моти", "type": "десерт"},
+            {"name": "сашими", "type": "основное"}
+        ]
+    },
+    {
+        "name": "Мясо & Огонь",
+        "cuisine": "европейская",
+        "year_opened": 2010,
+        "rating": 4.7,
+        "signature_dishes": [
+            {"name": "стейк рибай", "type": "основное"},
+            {"name": "карпаччо", "type": "закуска"}
+        ]
+    },
+    {
+        "name": "Веранда",
+        "cuisine": "средиземноморская",
+        "year_opened": 2020,
+        "rating": 4.6,
+        "signature_dishes": [
+            {"name": "паста карбонара", "type": "основное"},
+            {"name": "тирамису", "type": "десерт"},
+            {"name": "капрезе", "type": "закуска"}
+        ]
+    },
+    {
+        "name": "Чайхана",
+        "cuisine": "узбекская",
+        "year_opened": 2012,
+        "rating": 4.5,
+        "signature_dishes": [
+            {"name": "плов", "type": "основное"},
+            {"name": "самса", "type": "закуска"},
+            {"name": "шурпа", "type": "суп"},
+            {"name": "лагман", "type": "основное"}
+        ]
+    },
+    {
+        "name": "Франсуа",
+        "cuisine": "французская",
+        "year_opened": 2008,
+        "rating": 4.9,
+        "signature_dishes": [
+            {"name": "луковый суп", "type": "суп"},
+            {"name": "утиное филе", "type": "основное"},
+            {"name": "крем-брюле", "type": "десерт"},
+            {"name": "фуа-гра", "type": "закуска"}
+        ]
+    }
+]
---
+++ 01062026/theory.py
@@ -0,0 +1,19 @@
+
+
+HTTP протокол
+http-не защищенный
+https://example.com
+s - SSL сертификат (сертификат безопасности)
+request - запрос, response - ответ
+
+GET - получить
+POST - отправить (создать)
+
+status code
+1xx
+2xx
+3xx
+4xx
+5xx
+
+
